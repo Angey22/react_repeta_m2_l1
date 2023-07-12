b4981ed359229d0cd71754c640474186429fe00d
Angey22 <babandval@gmail.com>
2023-07-12 20:06:20 +0300

    1

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+// import { Component } from 'react'
+import Header from './Header/Header'
+import Counter from './Counter/Counter'
+// import ToDoList from './ToDoList/ToDoList'
+// import Modal from './Modal/Modal'
+
+const App = () => {
+	return (
+		<div className='container'>
+			<Header />
+			<Counter />
+		</div>
+	)
+}
+
+export default App
